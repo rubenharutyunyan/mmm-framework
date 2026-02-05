8d0131144eab13bdcc3c7269eac5bbd5cbe35597
rubenharutyunyan <86978846+rubenharutyunyan@users.noreply.github.com>
2026-02-05 17:10:57 +0100

    Update __init__.py

--- src/mmm/data/__init__.py
+++ src/mmm/data/__init__.py
@@ -1,1 +1,20 @@
-# Package marker
+# src/mmm/data/__init__.py
+
+from .column_mapper import ColumnMapper, MappingReport
+from .exceptions import (
+    ColumnMappingError,
+    InvalidTargetColumnNameError,
+    SourceColumnMissingError,
+    SourceNormalizationCollisionError,
+    TargetColumnCollisionError,
+)
+
+__all__ = [
+    "ColumnMapper",
+    "MappingReport",
+    "ColumnMappingError",
+    "InvalidTargetColumnNameError",
+    "SourceColumnMissingError",
+    "SourceNormalizationCollisionError",
+    "TargetColumnCollisionError",
+]
